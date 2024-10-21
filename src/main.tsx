571fd4b3df238e82408ecfb846310d9deff7da4d
Jan Eckerlein <eckerlein.jan@gmail.com>
2024-10-21 13:06:32 +0200

    tailwind init

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { RouterProvider, createRouter } from "@tanstack/react-router";
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
+import "./main.css";
 
 // Create a new router instance
 const router = createRouter({ routeTree });
