9876821c1d5945f9a92c212168e993b7a993c676
Jan Eckerlein <eckerlein.jan@gmail.com>
2024-10-23 07:17:02 +0200

    use @tauri-apps/plugin-store for contact

---
+++ src/features/contacts/contactStore.tsx
@@ -0,0 +1,24 @@
+import { load } from "@tauri-apps/plugin-store";
+import { Contact, contactSchema } from "./contactSchema";
+
+const contactStore = await load("store/contact.json");
+
+async function saveContact(contact: Contact) {
+  const { error, data } = contactSchema.safeParse(contact);
+  if (error) return { error };
+
+  const { id, ...rest } = data;
+  await contactStore.set(id, rest);
+}
+
+async function getContact(id: string): Promise<Contact | undefined> {
+  const data = await contactStore.get<Omit<Contact, "id">>(id);
+  if (!data) return;
+  return { id, ...data };
+}
+
+export default {
+	...contactStore,
+	saveContact,
+	getContact,
+}
