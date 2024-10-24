f5e05501e75b5db408c0ae574c9c34deac8187d2
Jan Eckerlein <eckerlein.jan@gmail.com>
2024-10-24 18:57:59 +0200

    contact list fix cities render

--- src/features/contacts/contactUtils.ts
+++ src/features/contacts/contactUtils.ts
@@ -1,3 +1,4 @@
+import { Contact } from "./contactSchema";
 
 export const getContactName = (
   contact: Contact | Omit<Contact, "id">
@@ -6,5 +7,15 @@ export const getContactName = (
     return contact.baseInfo?.type === "person"
       ? `${contact.baseInfo.firstName} ${contact.baseInfo.lastName}`
       : contact.baseInfo?.companyName;
+  }
+};
+
+// {contact.address?.map((address) => address.city).join(", ")}
+
+export const getContactCities = (
+  contact: Contact | Omit<Contact, "id">
+): string => {
+  {
+    return contact.address?.map((address) => address.city).join(", ") ?? "";
   }
 };
