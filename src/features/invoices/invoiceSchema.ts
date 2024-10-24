4d108e6bcbd5bd14f1c53bb345335259431966bb
Jan Eckerlein <eckerlein.jan@gmail.com>
2024-10-24 20:58:11 +0200

    feat: Add IncomingInvoiceForm component for creating incoming invoices

---
+++ src/features/invoices/invoiceSchema.ts
@@ -0,0 +1,30 @@
+import { z } from "zod";
+
+export const orderItemSchema = z.object({
+	description: z.string(),
+	unit: z.string(),
+	quantity: z.number(),
+	price: z.number(),
+})
+
+export const outgoingInvoiceSchema = z.object({
+	id: z.string(),
+	name: z.string(),
+	documentDate: z.date(),
+	amount: z.number(),
+	contact: z.string().optional(),
+	lineItems: z.array(orderItemSchema),
+	createdDate: z.date(),
+	editDate: z.date().optional(),
+	deleteDate: z.date().optional(),
+})
+
+export const incomingInvoiceSchema = z.object({
+	id: z.string(),
+	name: z.string(),
+	documentDate: z.date(),
+	amount: z.number(),
+	contact: z.string().optional(),
+	uploadedDocument: z.string(),
+	receivedDate: z.date(),
+})
--- src/routes/invoices/create.tsx
+++ src/routes/invoices/create.tsx
@@ -1,5 +1,42 @@
-import { createFileRoute } from '@tanstack/react-router'
+import PageHeader from "@/components/sections/PageHeader";
+import { Button } from "@/components/ui/button";
+import {
+  IncomingInvoiceForm,
+  IncomingInvoiceFormRef,
+} from "@/features/invoices/incomingInvoiceForm";
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
+import { useRef } from "react";
 
-export const Route = createFileRoute('/invoices/create')({
-  component: () => <div>Hello /invoices/create!</div>,
-})
+export const Route = createFileRoute("/invoices/create")({
+  component: () => {
+    const navigate = useNavigate();
+    const formRef = useRef<IncomingInvoiceFormRef>(null);
+
+    return (
+      <main>
+        <PageHeader
+          showBackButton={true}
+          title="Eingehende Rechnung aufnehmen"
+          actionBar={
+            <>
+              <Button
+                variant="default"
+                onClick={async () => {
+                  await formRef.current?.submit();
+                  // navigate({ to: "/contacts" });
+                }}
+              >
+                Speichern
+              </Button>
+            </>
+          }
+        />
+        <IncomingInvoiceForm
+          className="px-4 pt-4 pb-12"
+          ref={formRef}
+          showButton={false}
+        />
+      </main>
+    );
+  },
+});
