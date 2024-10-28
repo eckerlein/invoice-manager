2ab05ea85377debcb6cded92044e4918582efe81
Jan Eckerlein <eckerlein.jan@gmail.com>
2024-10-28 11:38:54 +0100

    feat: Add Tabs component for managing tabbed content
    The Tabs component has been added to the project. It allows for the management of tabbed content, providing a user-friendly way to switch between different sections of a page. This component enhances the overall user experience and improves the organization of information within the application.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,6 +4,7 @@ import InvoiceTable from "@/features/invoices/invoiceTable";
 import ContactTable from "@/features/contacts/contactTable";
 import { DashboardCard } from "@/features/dashboard/DashboardCard";
 import { FinancialChart } from "@/features/dashboard/FinancialChart";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export const Route = createFileRoute("/")({
   component: () => {
@@ -12,7 +13,15 @@ export const Route = createFileRoute("/")({
         <PageHeader title="Dashboard" size="lg" />
 
         <div className="grid grid-cols-2 gap-4 p-4">
-          <FinancialChart />
+          <Tabs defaultValue="last-6-months" className="w-full">
+            <TabsList className="w-full">
+              <TabsTrigger value="last-6-months">Letzte 6 Monate</TabsTrigger>
+              {/* <TabsTrigger value="last-year">Letztes Jahr</TabsTrigger> */}
+            </TabsList>
+            <TabsContent value="last-6-months">
+              <FinancialChart />
+            </TabsContent>
+          </Tabs>
           {/* <DashboardCard title="Rechnungen" link="/invoices/create">
             <InvoiceTable />
           </DashboardCard> */}
