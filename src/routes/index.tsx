a125359925ede5b5230eb4475c22278f6e75e4a0
Jan Eckerlein <eckerlein.jan@gmail.com>
2024-10-28 14:00:40 +0100

    Fix hight on dashboard

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 import PageHeader from "@/components/sections/PageHeader";
 import InvoiceTable from "@/features/invoices/invoiceTable";
-import ContactTable from "@/features/contacts/contactTable";
+import { useEffect, useState } from "react";
 import { DashboardCard } from "@/features/dashboard/DashboardCard";
 import { IncomeExpenseTrendChart } from "@/features/charts/IncomeExpenseTrendChart";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
@@ -10,8 +10,18 @@ import { BalanceOverTimeBigChart } from "@/features/charts/BalanceOverTimeBigCha
 
 export const Route = createFileRoute("/")({
   component: () => {
+    // State variable to force re-render
+    const [tabKey, setTabKey] = useState(0);
+
+    // Effect to listen for window resize and update tabKey
+    useEffect(() => {
+      const handleResize = () => setTabKey((prevKey) => prevKey + 1);
+      window.addEventListener("resize", handleResize);
+      return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
     return (
-      <main className="relative w-full min-h-screen overflow-y-auto flex flex-col">
+      <main className="relative w-full h-[max(100vh,800px)] flex flex-col">
         <PageHeader title="Dashboard" variants={{ position: "static" }} />
 
         <div className="h-full w-full p-4">
@@ -19,7 +29,8 @@ export const Route = createFileRoute("/")({
             <div className="row-span-3">
               <Tabs
                 defaultValue="financial-trends"
-                className="w-full h-full overflow-clip grid grid-rows-[auto_1fr] gap-2 "
+                key={tabKey} // Re-render Tabs on resize
+                className="w-full h-full grid grid-rows-[auto_1fr] gap-2"
               >
                 <TabsList className="w-full grid grid-cols-2">
                   <TabsTrigger value="financial-trends">Trends</TabsTrigger>
@@ -43,10 +54,6 @@ export const Route = createFileRoute("/")({
             </DashboardCard>
             <BalanceOverTimeBigChart className="col-span-2 row-span-2" />
           </div>
-
-          {/* <DashboardCard title="Kontakte" link="/contacts/create">
-            <ContactTable />
-          </DashboardCard> */}
         </div>
       </main>
     );
